1dbb82b1c9101309f508f4dcf4d516ec5ee23128
Alexy Thouin <2475806@cegepmv.ca>
2025-05-06 10:32:44 -0400

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
 export default function Header(){
-    <h1>haut</h1>
+    return ( 
+        <>
+            <div className="About-me">
+                <h1>Mon portfolio</h1>
+                <p>Nom: Alexy Thouin</p>
+                <p>Tel: 514-627-1105</p>
+                <p>Adresse: ----</p>
+            </div>
+        </>
+    )
 }
